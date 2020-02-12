3853828a0d29406ea7062e2b6e9a71e36965c66e
dss19 <samoylov82@gmail.com>
2020-02-12 19:58:19 +0300

    добавил сборку

---
+++ build/precache-manifest.c9f1df568d0887fb174deeb12cec62b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8fd07aa9e2798c464fc50a1ee1177a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e549abdedd5b8d0f478",
+    "url": "/static/css/main.356f782e.chunk.css"
+  },
+  {
+    "revision": "e17ca286f7a359118ae2",
+    "url": "/static/js/2.2857153b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2857153b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e549abdedd5b8d0f478",
+    "url": "/static/js/main.890dced2.chunk.js"
+  },
+  {
+    "revision": "8275f47b8b36a4f5ed3a",
+    "url": "/static/js/runtime-main.895393aa.js"
+  }
+]);
